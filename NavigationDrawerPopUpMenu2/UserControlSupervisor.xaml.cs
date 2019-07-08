e55136272ff78e8f687ab95490dd65eaaec127e5
Ede Bueno <edebueno@outlook.com>
2019-07-08 09:42:06 -0300

    Add project files.

---
+++ NavigationDrawerPopUpMenu2/UserControlSupervisor.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SIS_AS
+{
+    /// <summary>
+    /// Interação lógica para UserControlHome.xam
+    /// </summary>
+    public partial class UserControlSupervisor : UserControl
+    {
+        public UserControlSupervisor()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+    }
+}
